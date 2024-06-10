d91ed02e278ed9a104de8a5911593a9ea100885a
NADIA-PM24 <nadperezmendez@gmail.com>
2024-06-10 12:21:17 -0600

    avance de principio .js, .html, analyzer, css, llevó de porcentaje % 50 para terminat el proyecto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,39 @@
+/* eslint-disable no-unused-vars */
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount: (text) =>{
+    console.log(text);
+    return text.length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCount: (text) => {
+  getCharacterCount: (text) =>{
+    console.log(text);
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCountExcludingSpaces: (text) => {
+  getCharacterCountExcludingSpaces: (text) =>{
+    console.log(text);
+    for (let i = 0; i < text.length; i++) {
+      if (/\w/.test(text[i])) {
+        console.log (text[i])
+      }
+    }
+
+
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) =>{
+    console.log(text);
+    return text.length;   
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberCount: (text) => {
+  getNumberCount: (text) =>{
+    console.log(text)
+    return text.length;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberSum: (text) => {
+  getNumberSum: (text) =>{
+    console.log(text);
+    return text.sum;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import analyzer from './analyzer.js';
+console.log(analyzer)
+  
+const textarea=document.querySelector("textarea");
+textarea.addEventListener("input",function() {
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+  const palabras = document.querySelector('li[data-testid="word-count"]');
+  const caracteres = document.querySelector('li[data-testid="character-count"]');
+  const espacios = document.querySelector('li[data-testid="no-spaces-count"]');
+  const numeros = document.querySelector('li[data-testid="number-count"]');
+  const suma = document.querySelector('li[data-testid="number-sum"]');
+  const longitud = document.querySelector('li[data-testid="average-word-length"]');
+
+  const characterCount = analyzer.getCharacterCount(textarea.value);
+  caracteres.textContent = "caracteres" + characterCount
+  
+  const CharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value)
+  espacios.textContent = "espacios" + CharacterCountExcludingSpaces
+
+
+});
